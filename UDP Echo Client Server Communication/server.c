cca0ee85c177781132b0e5ef9d513385784444b9
sairohitp <sairohitp007@gmail.com>
2023-08-25 09:14:47 +0530

    exp4

--- UDP Echo Client Server Communication/server.c
+++ UDP Echo Client Server Communication/server.c
@@ -1,35 +1,44 @@
-#include <iostream>
-#include <cstring>
-#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
 #include <sys/socket.h>
-#include <unistd.h> // Include this for the 'close' function
+#include <netinet/in.h>
+#include <unistd.h>
 
-void udp_echo_server(int port) {
-    int serverSocket = socket(AF_INET, SOCK_DGRAM, 0);
+// time
 
-    struct sockaddr_in serverAddr, clientAddr;
-    memset(&serverAddr, 0, sizeof(serverAddr));
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
-    serverAddr.sin_port = htons(port);
-
-    bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
-
-    std::cout << "UDP Echo Server is listening on port " << port << std::endl;
-
-    while (true) {
-        char buffer[1024];
-        socklen_t clientAddrLen = sizeof(clientAddr);
-        ssize_t recvLen = recvfrom(serverSocket, buffer, sizeof(buffer), 0, (struct sockaddr*)&clientAddr, &clientAddrLen);
-        sendto(serverSocket, buffer, recvLen, 0, (struct sockaddr*)&clientAddr, clientAddrLen);
-    }
-
-    close(serverSocket);
-}
+#define MAXLINE 1024
+#define PORT 5035
 
 int main() {
-    int port = 12345;
-    udp_echo_server(port);
 
+    int socketDescriptor = socket(AF_INET, SOCK_DGRAM, 0);
+    int number;
+    socklen_t addressLength;
+    char message[MAXLINE];
+
+    struct sockaddr_in serverAddress, clientAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(PORT);
+
+    bind(socketDescriptor, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+
+    printf("\nServer Started ...\n");
+
+    while (1)
+    {
+        printf("\n");
+        addressLength = sizeof(clientAddress);
+
+        number = recvfrom(socketDescriptor, message, MAXLINE, 0, (struct sockaddr *)&clientAddress, &addressLength);
+
+        printf("\n Client's Message: %s ", message);
+
+        if (number < 6)
+            perror("send error");
+
+        sendto(socketDescriptor, message, number, 0, (struct sockaddr *)&clientAddress, addressLength);
+    }
     return 0;
 }
